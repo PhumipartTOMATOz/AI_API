2a8abf37822a90088dad58b6c417383f2f8bf462
PhumipartTOMATOz <Phumipart.VIS@gmail.com>
2024-10-09 18:16:03 +0700

    edit flask

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ def train_model():
     #         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
